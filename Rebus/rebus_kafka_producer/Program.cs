2eb7adf8b07a6a3f5d1603cafe5f3a5e767bebbc
Nicklas Peistrup <januspeis@gmail.com>
2022-11-21 19:26:11 +0100

    qs

--- Rebus/rebus_kafka_producer/Program.cs
+++ Rebus/rebus_kafka_producer/Program.cs
@@ -1,26 +1,102 @@
-﻿using Rebus.Activation;
-using Rebus.Config;
+﻿using System.Diagnostics;
+using Confluent.Kafka;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Rebus.Kafka;
-using Rebus.Routing.TypeBased;
-using shared_stuff;
+using Serilog;
 
-using var activator = new BuiltinHandlerActivator();
-
-Configure.With(activator)
-    .Transport(t => t.UseKafkaAsOneWayClient(ConstStrings.kafkaUrl))
-    // .Transport(t => t.UseKafka(ConstStrings.kafkaUrl,ConstStrings.topic,ConstStrings.topic))
-    .Routing(x => x.TypeBased())
-    .Logging(x=>x.Console())
-    .Start();
-
-
-foreach (int i in Enumerable.Range(0,10))
+namespace ProducerConsumer
 {
-    
-    Console.WriteLine("sending stuff");
-    activator.Bus.Publish(new Step1Event
-    {
-        Counter =i 
-    });    
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CancellationTokenSource cts = new CancellationTokenSource();
+			Console.CancelKeyPress += (_, e) =>
+			{
+				e.Cancel = true; // prevent the process from terminating.
+				cts.Cancel();
+			};
+			var globalScope = ConfigureServices();
+			var loggerFactory = globalScope.ServiceProvider.GetRequiredService<ILoggerFactory>();
+			ILogger<KafkaProducer> loggerProducer = loggerFactory.CreateLogger<KafkaProducer>();
+			var loggerConsumer = loggerFactory.CreateLogger<KafkaConsumer>();
+
+			using (var dependentKafkaProducer = new KafkaProducer(_kafkaEndpoint, loggerProducer))
+			using (var producer = new KafkaProducer(dependentKafkaProducer)) // for test dependentKafkaProducer 
+			using (KafkaConsumer consumer = new KafkaConsumer(_kafkaEndpoint, loggerConsumer)
+				, consumer2 = new KafkaConsumer(_kafkaEndpoint, loggerConsumer))
+			{
+				consumer.Consume(new[] { bTopicNameResp })
+					.Subscribe(message => Console.WriteLine($"Boy name {message.Value} is recommended"), cts.Token);
+				consumer2.Consume(new[] { gTopicNameResp })
+					.Subscribe(message => Console.WriteLine($"Girl name {message.Value} is recommended"), cts.Token);
+
+				string userInput;
+				var rnd = new Random();
+				do
+				{
+					Console.WriteLine(userHelpMsg);
+					userInput = Console.ReadLine();
+					switch (userInput)
+					{
+						case "b":
+							var nameCount = 1000;
+							Task[] jobs = Enumerable.Range(0, nameCount)
+								.Select(i => new Message<Null, string> { Value = $"{i:D4} {_boyNames[rnd.Next(0, 5)]}" })
+								.Select(m => producer.ProduceAsync(bTopicNameResp, m))
+								.ToArray();
+							Stopwatch sw = Stopwatch.StartNew();
+							Task.WaitAll(jobs);
+							Console.WriteLine($"Sending {nameCount} за {sw.ElapsedMilliseconds / 1000:N3}с");
+							break;
+						case "g":
+							producer.ProduceAsync(gTopicNameResp, new Message<Null, string> { Value = _girlNames[rnd.Next(0, 5)] }
+								, cts.Token).GetAwaiter().GetResult();
+							break;
+						case "q":
+							break;
+						default:
+							Console.WriteLine($"Unknown command.");
+							break;
+					}
+				} while (userInput != "q");
+			}
+		}
+
+		private static IServiceScope ConfigureServices()
+		{
+			var logger = Log.Logger = new LoggerConfiguration()
+				.WriteTo.Console()
+				.CreateLogger();
+
+			var services = new ServiceCollection();
+			services.AddLogging(builder => builder.AddSerilog(logger, true));
+
+			return services.BuildServiceProvider().CreateScope();
+		}
+
+		static readonly string _kafkaEndpoint = "127.0.0.1:9092";
+		private static readonly string bTopicNameResp = "b_name_response";
+		private static readonly string gTopicNameResp = "g_name_response";
+		private static readonly string[] _boyNames =
+		{
+			"Arsenii",
+			"Igor",
+			"Kostya",
+			"Ivan",
+			"Dmitrii",
+		};
+
+		private static readonly string[] _girlNames =
+		{
+			"Nastya",
+			"Lena",
+			"Ksusha",
+			"Katya",
+			"Olga"
+		};
+		private static readonly string userHelpMsg =
+			"Enter 'b' or 'g' to process boy or girl names respectively. Press q to exit.";
+	}
 }
-Console.WriteLine("Done");
---
+++ Rebus/rebus_kafka_producer/Program_back.cs
@@ -0,0 +1,26 @@
+﻿// using Rebus.Activation;
+// using Rebus.Config;
+// using Rebus.Kafka;
+// using Rebus.Routing.TypeBased;
+// using shared_stuff;
+//
+// using var activator = new BuiltinHandlerActivator();
+//
+// Configure.With(activator)
+//     .Transport(t => t.UseKafkaAsOneWayClient(ConstStrings.kafkaUrl))
+//     // .Transport(t => t.UseKafka(ConstStrings.kafkaUrl,ConstStrings.topic,ConstStrings.topic))
+//     .Routing(x => x.TypeBased())
+//     .Logging(x=>x.Console())
+//     .Start();
+//
+//
+// foreach (int i in Enumerable.Range(0,10))
+// {
+//     
+//     Console.WriteLine("sending stuff");
+//     activator.Bus.Publish(new Step1Event
+//     {
+//         Counter =i 
+//     });    
+// }
+// Console.WriteLine("Done");
