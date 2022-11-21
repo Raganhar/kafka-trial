714077365338caef935645858bd9e2c7ca8b43f1
Nicklas Peistrup <januspeis@gmail.com>
2022-11-21 23:31:31 +0100

    qs

--- ConfluentKafka/confluent_kafka_consumer/Program.cs
+++ ConfluentKafka/confluent_kafka_consumer/Program.cs
@@ -3,46 +3,211 @@
 using Confluent.Kafka;
 using shared_stuff;
 
-var conf = new ConsumerConfig
+public class Program
 {
-    GroupId = ConstStrings.kafkaGroup,
-    BootstrapServers = ConstStrings.kafkaUrl,
-    // Note: The AutoOffsetReset property determines the start offset in the event
-    // there are not yet any committed offsets for the consumer group for the
-    // topic/partitions of interest. By default, offsets are committed
-    // automatically, so in this example, consumption will only start from the
-    // earliest message in the topic 'my-topic' the first time you run the program.
-    AutoOffsetReset = AutoOffsetReset.Earliest,
-};
-
-using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
-{
-    c.Subscribe(ConstStrings.topic);
-    CancellationTokenSource cts = new CancellationTokenSource();
-    Console.CancelKeyPress += (_, e) =>
+    /// <summary>
+    ///     In this example
+    ///         - offsets are automatically committed.
+    ///         - no extra thread is created for the Poll (Consume) loop.
+    /// </summary>
+    public static void Run_Consume(string brokerList, List<string> topics, CancellationToken cancellationToken)
     {
-        e.Cancel = true; // prevent the process from terminating.
-        cts.Cancel();
-    };
-
-    try
-    {
-        while (true)
+        var config = new ConsumerConfig
         {
+            BootstrapServers = brokerList,
+            GroupId = "csharp-consumer",
+            EnableAutoOffsetStore = false,
+            EnableAutoCommit = true,
+            StatisticsIntervalMs = 5000,
+            SessionTimeoutMs = 6000,
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+            EnablePartitionEof = true,
+            // A good introduction to the CooperativeSticky assignor and incremental rebalancing:
+            // https://www.confluent.io/blog/cooperative-rebalancing-in-kafka-streams-consumer-ksqldb/
+            PartitionAssignmentStrategy = PartitionAssignmentStrategy.CooperativeSticky
+        };
+
+        // Note: If a key or value deserializer is not set (as is the case below), the 
+        // deserializer corresponding to the appropriate type from Confluent.Kafka.Deserializers
+        // will be used automatically (where available). The default deserializer for string
+        // is UTF8. The default deserializer for Ignore returns null for all input data
+        // (including non-null data).
+        using (var consumer = new ConsumerBuilder<Ignore, string>(config)
+                   // Note: All handlers are called on the main .Consume thread.
+                   .SetErrorHandler((_, e) => Console.WriteLine($"Error: {e.Reason}"))
+                   // .SetStatisticsHandler((_, json) => Console.WriteLine($"Statistics: {json}"))
+                   // .SetPartitionsAssignedHandler((c, partitions) =>
+                   // {
+                   //     // Since a cooperative assignor (CooperativeSticky) has been configured, the
+                   //     // partition assignment is incremental (adds partitions to any existing assignment).
+                   //     Console.WriteLine(
+                   //         "Partitions incrementally assigned: [" +
+                   //         string.Join(',', partitions.Select(p => p.Partition.Value)) +
+                   //         "], all: [" +
+                   //         string.Join(',', c.Assignment.Concat(partitions).Select(p => p.Partition.Value)) +
+                   //         "]");
+                   //
+                   //     // Possibly manually specify start offsets by returning a list of topic/partition/offsets
+                   //     // to assign to, e.g.:
+                   //     // return partitions.Select(tp => new TopicPartitionOffset(tp, externalOffsets[tp]));
+                   // })
+                   // .SetPartitionsRevokedHandler((c, partitions) =>
+                   // {
+                   //     // Since a cooperative assignor (CooperativeSticky) has been configured, the revoked
+                   //     // assignment is incremental (may remove only some partitions of the current assignment).
+                   //     var remaining = c.Assignment.Where(atp =>
+                   //         partitions.Where(rtp => rtp.TopicPartition == atp).Count() == 0);
+                   //     Console.WriteLine(
+                   //         "Partitions incrementally revoked: [" +
+                   //         string.Join(',', partitions.Select(p => p.Partition.Value)) +
+                   //         "], remaining: [" +
+                   //         string.Join(',', remaining.Select(p => p.Partition.Value)) +
+                   //         "]");
+                   // })
+                   // .SetPartitionsLostHandler((c, partitions) =>
+                   // {
+                   //     // The lost partitions handler is called when the consumer detects that it has lost ownership
+                   //     // of its assignment (fallen out of the group).
+                   //     Console.WriteLine($"Partitions were lost: [{string.Join(", ", partitions)}]");
+                   // })
+                   .Build())
+        {
+            consumer.Subscribe(topics);
+
             try
             {
-                var cr = c.Consume(cts.Token);
-                Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                while (true)
+                {
+                    try
+                    {
+                        var consumeResult = consumer.Consume(cancellationToken);
+
+                        if (consumeResult.IsPartitionEOF)
+                        {
+                            Console.WriteLine(
+                                $"Reached end of topic {consumeResult.Topic}, partition {consumeResult.Partition}, offset {consumeResult.Offset}.");
+
+                            continue;
+                        }
+
+                        Console.WriteLine(
+                            $"Received message at {consumeResult.TopicPartitionOffset}: {consumeResult.Message.Value}");
+                        try
+                        {
+                            // Store the offset associated with consumeResult to a local cache. Stored offsets are committed to Kafka by a background thread every AutoCommitIntervalMs. 
+                            // The offset stored is actually the offset of the consumeResult + 1 since by convention, committed offsets specify the next message to consume. 
+                            // If EnableAutoOffsetStore had been set to the default value true, the .NET client would automatically store offsets immediately prior to delivering messages to the application. 
+                            // Explicitly storing offsets after processing gives at-least once semantics, the default behavior does not.
+                            consumer.StoreOffset(consumeResult);
+                        }
+                        catch (KafkaException e)
+                        {
+                            Console.WriteLine($"Store Offset error: {e.Error.Reason}");
+                        }
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Consume error: {e.Error.Reason}");
+                    }
+                }
             }
-            catch (ConsumeException e)
+            catch (OperationCanceledException)
             {
-                Console.WriteLine($"Error occured: {e.Error.Reason}");
+                Console.WriteLine("Closing consumer.");
+                consumer.Close();
             }
         }
     }
-    catch (OperationCanceledException)
+
+    /// <summary>
+    ///     In this example
+    ///         - consumer group functionality (i.e. .Subscribe + offset commits) is not used.
+    ///         - the consumer is manually assigned to a partition and always starts consumption
+    ///           from a specific offset (0).
+    /// </summary>
+    public static void Run_ManualAssign(string brokerList, List<string> topics, CancellationToken cancellationToken)
     {
-        // Ensure the consumer leaves the group cleanly and final offsets are committed.
-        c.Close();
+        var config = new ConsumerConfig
+        {
+            // the group.id property must be specified when creating a consumer, even 
+            // if you do not intend to use any consumer group functionality.
+            GroupId = "groupid-not-used-but-mandatory",
+            BootstrapServers = brokerList,
+            // partition offsets can be committed to a group even by consumers not
+            // subscribed to the group. in this example, auto commit is disabled
+            // to prevent this from occurring.
+            EnableAutoCommit = false
+        };
+
+        using (var consumer =
+               new ConsumerBuilder<Ignore, string>(config)
+                   .SetErrorHandler((_, e) => Console.WriteLine($"Error: {e.Reason}"))
+                   .Build())
+        {
+            consumer.Assign(topics.Select(topic => new TopicPartitionOffset(topic, 0, Offset.Beginning)).ToList());
+
+            try
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var consumeResult = consumer.Consume(cancellationToken);
+                        // Note: End of partition notification has not been enabled, so
+                        // it is guaranteed that the ConsumeResult instance corresponds
+                        // to a Message, and not a PartitionEOF event.
+                        Console.WriteLine(
+                            $"Received message at {consumeResult.TopicPartitionOffset}: ${consumeResult.Message.Value}");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Consume error: {e.Error.Reason}");
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Closing consumer.");
+                consumer.Close();
+            }
+        }
+    }
+
+    private static void PrintUsage()
+        => Console.WriteLine("Usage: .. <subscribe|manual> <broker,broker,..> <topic> [topic..]");
+
+    public static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            PrintUsage();
+            return;
+        }
+
+        var mode = args[0];
+        var brokerList = ConstStrings.kafkaUrl;// args[1];
+        var topics =new List<string>{ConstStrings.topic};// args.Skip(2).ToList();
+
+        Console.WriteLine($"Started consumer, Ctrl-C to stop consuming");
+
+        CancellationTokenSource cts = new CancellationTokenSource();
+        Console.CancelKeyPress += (_, e) =>
+        {
+            e.Cancel = true; // prevent the process from terminating.
+            cts.Cancel();
+        };
+
+        switch (mode)
+        {
+            case "subscribe":
+                Run_Consume(brokerList, topics, cts.Token);
+                break;
+            case "manual":
+                Run_ManualAssign(brokerList, topics, cts.Token);
+                break;
+            default:
+                PrintUsage();
+                break;
+        }
     }
 }
--- ConfluentKafka/confluent_kafka_producer/Program.cs
+++ ConfluentKafka/confluent_kafka_producer/Program.cs
@@ -1,26 +1,94 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Confluent.Kafka;
+using Confluent.Kafka.Admin;
 using shared_stuff;
 
-var config = new ProducerConfig { BootstrapServers = ConstStrings.kafkaUrl,Partitioner = Partitioner.Murmur2Random};
-
-// If serializers are not specified, default serializers from
-// `Confluent.Kafka.Serializers` will be automatically used where
-// available. Note: by default strings are encoded as UTF8.
-using (var p = new ProducerBuilder<Null, string>(config).Build())
+public class Program
 {
-    try
+    public static async Task Main(string[] args)
     {
-     
-        foreach (var i in Enumerable.Range(0,100))
+        // if (args.Length != 2)
+        // {
+        //     Console.WriteLine("Usage: .. brokerList topicName");
+        //     return;
+        // }
+
+        string brokerList = ConstStrings.kafkaUrl; // args[0];
+        string topicName = ConstStrings.topic; // args[1];
+
+
+        using (var adminClient =
+               new AdminClientBuilder(new AdminClientConfig { BootstrapServers = brokerList }).Build())
         {
-            var dr = await p.ProduceAsync(ConstStrings.topic, new Message<Null, string> { Value = "test" });
-            Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");    
+            try
+            {
+                await adminClient.DeleteTopicsAsync(new[] { topicName });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            Thread.Sleep(1000);
+            try
+            {
+                await adminClient.CreateTopicsAsync(new TopicSpecification[]
+                {
+                    new TopicSpecification { Name = topicName, ReplicationFactor = 1, NumPartitions = 3 }
+                });
+            }
+            catch (CreateTopicsException e)
+            {
+                Console.WriteLine($"An error occured creating topic {e.Results[0].Topic}: {e.Results[0].Error.Reason}");
+            }
         }
-    }
-    catch (ProduceException<Null, string> e)
-    {
-        Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+
+        var config = new ProducerConfig { BootstrapServers = brokerList, Partitioner = Partitioner.ConsistentRandom, };
+
+        using (var producer = new ProducerBuilder<string, string>(config)
+                   .Build())
+        {
+            Console.WriteLine("\n-----------------------------------------------------------------------");
+            Console.WriteLine($"Producer {producer.Name} producing on topic {topicName}.");
+            Console.WriteLine("-----------------------------------------------------------------------");
+            // Console.WriteLine("To create a kafka message with UTF-8 encoded key and value:");
+            // Console.WriteLine("> key value<Enter>");
+            // Console.WriteLine("To create a kafka message with a null key and UTF-8 encoded value:");
+            // Console.WriteLine("> value<enter>");
+            // Console.WriteLine("Ctrl-C to quit.\n");
+
+            var cancelled = false;
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true; // prevent the process from terminating.
+                cancelled = true;
+            };
+
+            while (!cancelled)
+            {
+                try
+                {
+                    // Note: Awaiting the asynchronous produce request below prevents flow of execution
+                    // from proceeding until the acknowledgement from the broker is received (at the 
+                    // expense of low throughput).
+                    foreach (var i in Enumerable.Range(0, 50))
+                    {
+                        var deliveryReport = await producer.ProduceAsync(
+                            topicName, new Message<string, string> { Key = $"Key: {i}", Value = i.ToString() });
+
+                        Console.WriteLine($"delivered to: {deliveryReport.TopicPartitionOffset}");
+                    }
+                }
+                catch (ProduceException<string, string> e)
+                {
+                    Console.WriteLine($"failed to deliver message: {e.Message} [{e.Error.Code}]");
+                }
+            }
+
+            // Since we are producing synchronously, at this point there will be no messages
+            // in-flight and no delivery reports waiting to be acknowledged, so there is no
+            // need to call producer.Flush before disposing the producer.
+        }
     }
 }
